538a791345695826f817ff9657586de0c19f47b0
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-09-04 21:50:14 -0300

    Applying hard coded url on account endpoints

--- library_front/src/Services/Axios/accountService.js
+++ library_front/src/Services/Axios/accountService.js
@@ -5,7 +5,7 @@ export async function registerAccount(
     firstName, lastName, email, username, password
   ) {
     try {
-      const response = await axios.post('auth/register/', {
+      const response = await axios.post('https://library-gces-back.herokuapp.com/auth/register/', {
         first_name: firstName,
         last_name: lastName,
         email: email,
@@ -26,7 +26,7 @@ export async function login(
   username, password
 ) {
   try {
-    const response = await axios.post('auth/login/', {
+    const response = await axios.post('https://library-gces-back.herokuapp.com/auth/login/', {
       username: username,
       password: password
     });
