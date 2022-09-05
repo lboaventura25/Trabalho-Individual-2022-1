bf9e29d91900764525ceb410780b2c625f0a92fc
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-09-04 21:27:06 -0300

    Adding hardcoded url to services on front [skip ci]

--- library_front/src/Services/Axios/bookService.js
+++ library_front/src/Services/Axios/bookService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_API } from './baseService';
+// import { BASE_API } from './baseService';
 
 export async function createBook(
     title, author, release_year, user
@@ -25,7 +25,7 @@ export async function createBook(
 
 export async function getBooks() {
     try {
-      const response = await BASE_API.get('https://library-gces-back.herokuapp.com/api/books/');
+      const response = await axios.get('https://library-gces-back.herokuapp.com/api/books/');
       return response;
     } catch (error) {
       if (error.response?.status === 500) {
